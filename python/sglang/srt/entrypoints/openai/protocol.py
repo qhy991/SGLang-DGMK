8d574da12d8c460c0f2c170cfa463d87d59150a7
majianping <majianping@infini-ai.com>
2026-04-08 11:18:34 +0800

    [feat]: add _input as role

--- python/sglang/srt/entrypoints/openai/protocol.py
+++ python/sglang/srt/entrypoints/openai/protocol.py
@@ -487,7 +487,7 @@ class ToolCall(BaseModel):
 
 
 class ChatCompletionMessageGenericParam(BaseModel):
-    role: Literal["system", "assistant", "tool", "function", "developer"]
+    role: Literal["system", "assistant", "tool", "function", "developer", "_input"]
     content: Union[str, List[ChatCompletionMessageContentPart], None] = Field(
         default=None
     )
@@ -502,9 +502,9 @@ class ChatCompletionMessageGenericParam(BaseModel):
     def _normalize_role(cls, v):
         if isinstance(v, str):
             v_lower = v.lower()
-            if v_lower not in {"system", "assistant", "tool", "function", "developer"}:
+            if v_lower not in {"system", "assistant", "tool", "function", "developer", "_input"}:
                 raise ValueError(
-                    "'role' must be one of 'system', 'developer', 'assistant', 'tool', or 'function' (case-insensitive)."
+                    "'role' must be one of 'system', 'developer', 'assistant', 'tool', 'function', or '_input' (case-insensitive)."
                 )
             return v_lower
         raise ValueError("'role' must be a string")
